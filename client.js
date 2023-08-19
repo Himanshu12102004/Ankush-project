45cfb20077af96b1681e8f0caf2c70253e3a037f
himanshu gupta <himanshugupta@himanshus-MacBook-Air.local>
2023-08-19 08:44:02 +0530

    debugged

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:3000"); // Change URL if needed
+const socket = new WebSocket("https://gdsc-project-0mb7.onrender.com"); // Change URL if needed
 
 const messagesContainer = document.getElementById("messagesContainer");
 const messageInput = document.getElementById("messageInp");
